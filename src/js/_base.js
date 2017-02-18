8e165a9aaafd933f3cfc78ac0c8d2483017f9c63
Nick Prozorovsky <nick.prozorovsky@gmail.com>
2017-02-18 16:16:00 -0600

    fixing responsiveness

--- src/js/_base.js
+++ src/js/_base.js
@@ -5,24 +5,34 @@
 */
 $(document).ready(function() {
 
+    // Fade body content in after page has loaded
+    $('body').css('visibility','visible').hide().fadeIn('fast');
+
 	/**
 	 * Responsive utility for determining if the current window is a desktop viewport.
+	 * @returns {boolean} whether or not the window is a desktop viewport
 	 */
-	var desktopBreakpoint = 767;
+	var desktopBreakpoint = 1024;
 	var isDesktop = function() {
 		return $(window).width() > desktopBreakpoint;
 	};
 
-	// Fade body content in after page has loaded
-	$('body').css('visibility','visible').hide().fadeIn('fast');
+    /**
+	 * Responsive utility for determining if the current window is a landscape viewport.
+     * @returns {boolean} whether or not the window is a landscape viewport
+     */
+	var isLandscape = function() {
+		return window.innerHeight < window.innerWidth;
+	};
 
-	if(window.innerHeight < window.innerWidth) {
-		$('.section').addClass('fp-auto-height');
-	}
-
+    /**
+	 * Event listener for orientation changes. takes action when orientation changes on the device.
+     */
     window.addEventListener("orientationchange", function() {
-        // Announce the new orientation number
-        $('.section').addClass('fp-auto-height');
-        $('.section').height(window.innerHeight - 50);
+        if(isLandscape()) {
+
+		} else {
+
+		}
     }, false);
 });
